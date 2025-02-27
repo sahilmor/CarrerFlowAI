f7d5527f78f99f89a5119f11eb9b582e4db86b5e
MrZorawaR <nitishshyoran420@gmail.com>
2025-02-28 03:41:00 +0530

    login done

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ const LandingPage = () => {
                 <ArrowRight className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
               </Button>
             </Link>
-            <Link href="/login">
+            <Link href="/sign-in">
               <Button>
                 Sign In
               </Button>
